2bdeed2cc8372ced6955500fe4d42d09cf427409
1Stohk1 <chrispeluso98@gmail.com>
2023-05-26 13:04:48 +0200

    Added the control on the conditioning of the matrix

--- OLDConjugateGradient.m
+++ OLDConjugateGradient.m
@@ -1,6 +1,6 @@
-temp = csvread("Documenti/Matlab Scripts/ML-CUP22-TR.csv", 8);
+temp = csvread('ML-CUP22-TR.csv', 8);
 A = temp(:,2:10);
-b = temp(:,11);
+b = temp(:,11:12);
 
 v = 1:size(A,2);
 c = nchoosek(v,2);
@@ -13,14 +13,26 @@ for i = c:size(c,1)
     k = c(i, 2);
     NewCol = abs(A(:,j).*A(:,k));
     NewCol = NewCol/norm(NewCol);
-    A = [A NewCol];
+    NewCol1 = A(:,j).^2;
+    NewCol1 = NewCol1/norm(NewCol1);
+    A = [A NewCol NewCol1];
 end
 
 disp('Condition number of A after Column additioning')
 cond(A)
 
-new_A = A'*A;
-new_b = A'*b;
+sim_A = A'*A;
+D = diag(sim_A);
+
+C = sqrt(inv(diag(D)))*sim_A*sqrt(inv(diag(D)));
+alpha = min(eig(C));
+%alpha = 1e-16
+
+new_A = sim_A+alpha*D;
+%new_A = sim_A;
+
+new_b = (A'*b)+alpha*D;
+%new_b = A'*b;
 
 disp('Condition number of final A')
 cond(new_A)
@@ -32,12 +44,13 @@ catch ME
 end
 
 x1 = pcg(new_A,new_b(:,1));
-%x2 = pcg(new_A,new_b(:,2));
-%x = [x1 x2];
+x2 = pcg(new_A,new_b(:,2));
+x = [x1 x2];
 
-residual = new_A*x1 - new_b;
+residual = new_A*x - new_b;
 
 sqrt(norm(residual))
 if sqrt(norm(residual))<1000
     disp('Bella roba')
 end
+
